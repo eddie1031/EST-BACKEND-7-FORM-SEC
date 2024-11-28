acaf9e9b1aaf9bafbf02e42f7e0e4112f4de44d4
Eddie Woo <archaco123@gmail.com>
2024-11-28 19:21:12 +0900

    1강 - 폼 기반 인증 시큐리티 예제 세팅

---
+++ src/test/java/com/est/form_security/FormSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.form_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
