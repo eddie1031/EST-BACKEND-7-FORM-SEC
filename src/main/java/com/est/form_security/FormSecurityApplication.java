58d18bf2bad65867440eca26971a4e22ecff2509
Eddie Woo <archaco123@gmail.com>
2024-11-28 19:21:43 +0900

    ----

---
+++ src/main/java/com/est/form_security/FormSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.est.form_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormSecurityApplication.class, args);
+    }
+
+}
